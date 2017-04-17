65061b93adf167a1c406a6f6c74c3ce6da3fafcf
likaiyue <liky_13234038246@163.com>
2017-04-17 12:15:39 +0800

    first

---
+++ Sign-Language-Recognition-HMM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sign_Language_Recognition_HMM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
